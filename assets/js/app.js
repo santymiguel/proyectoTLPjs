d314e7b3ab3e850bfde96c01ed9d8f831d92dacd
santiago-ramirez <santymiguel311@gmail.com>
2026-06-04 16:20:17 -0300

    estructura basica

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+const personajes = [
+  {
+    id: 1,
+    nombre: "A-Bomb",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/m/1-a-bomb.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Abe Sapien",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/m/2-abe-sapien.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Abin Sur",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/m/3-abin-sur.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Abomination",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/4-abomination.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Abraxas",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg",
+  },
+];
