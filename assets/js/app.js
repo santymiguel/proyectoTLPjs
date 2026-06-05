b5b942ff29609bdbf4030acdf038e2ca314f662d
santiago-ramirez <santymiguel311@gmail.com>
2026-06-05 14:34:19 -0300

    se creo la clase card

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,19 +3,19 @@ const personajes = [
     id: 1,
     nombre: "A-Bomb",
     imagen:
-      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/m/1-a-bomb.jpg",
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/1-a-bomb.jpg",
   },
   {
     id: 2,
     nombre: "Abe Sapien",
     imagen:
-      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/m/2-abe-sapien.jpg",
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/2-abe-sapien.jpg",
   },
   {
     id: 3,
     nombre: "Abin Sur",
     imagen:
-      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/m/3-abin-sur.jpg",
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/3-abin-sur.jpg",
   },
   {
     id: 4,
@@ -30,3 +30,18 @@ const personajes = [
       "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg",
   },
 ];
+const contenedor = document.querySelector("#rowContainer");
+// personajes.forEach
+personajes.forEach((personaje) => {
+  console.log(personaje);
+  // console.log("sdadads");
+  const card = `
+    <div class="card" style="width: 18rem; margin:10px;">
+      <img src="${personaje.imagen}" class="card-img-top" alt="${personaje.nombre}">
+      <div class="card-body">
+        <h5 class="card-title">${personaje.nombre}</h5>
+      </div>
+    </div>
+  `;
+  contenedor.innerHTML += card;
+});
